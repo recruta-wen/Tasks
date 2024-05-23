3b1a4973902e89452d47484da469ffddf6e0f5eb
aizelli <alexandre.izelli@gmail.com>
2024-05-23 08:37:20 -0300

    criado tela tasklist

---
+++ src/screens/Task_List.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react"
+import { StyleSheet, View, Text, ImageBackground } from "react-native"
+
+import today_Image from '../../assets/imgs/today.jpg'
+
+export default class Task_List extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <ImageBackground source={today_Image} style={styles.background}>
+
+                </ImageBackground>
+                <View style={styles.taskList}>
+                    <Text>Lista de Tarefas</Text>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }, 
+    background:{
+        flex: 3
+    },
+    taskList:{
+        flex: 7
+    }
+})
