2349ef6fe858e25cab2c9b577162cd94f07300ad
aizelli <alexandre.izelli@gmail.com>
2024-06-10 07:15:55 -0300

    criado método de filtro

--- src/screens/Task_List.js
+++ src/screens/Task_List.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { StyleSheet, View, Text, ImageBackground, FlatList, TouchableOpacity, Platform} from "react-native"
+import { StyleSheet, View, Text, ImageBackground, FlatList, TouchableOpacity, Platform } from "react-native"
 import Icon from "react-native-vector-icons/FontAwesome6"
 
 import moment from "moment"
@@ -14,6 +14,7 @@ export default class Task_List extends Component {
 
     state = {
         show_done_task: true,
+        visible_tasks: [],
         tasks: [{
             id: Math.random(),
             description: "Comprar jogo GTA 6",
@@ -34,8 +35,24 @@ export default class Task_List extends Component {
         }]
     }
 
-    toggle_filter = () =>{
-        this.setState({ show_done_task: !this.state.show_done_task })
+    componentDidMount = () => {
+        this.filter_tasks()
+    }
+
+    toggle_filter = () => {
+        this.setState({ show_done_task: !this.state.show_done_task }, this.filter_tasks)
+    }
+
+    filter_tasks = () => {
+        let visible_tasks = null
+        if(this.state.show_done_task){
+            visible_tasks = [...this.state.tasks]
+        }else{
+            const pending = task => task.done_at === null
+            visible_tasks = this.state.tasks.filter(pending)
+        }
+
+        this.setState({visible_tasks})
     }
 
     toggle_task = task_id => {
@@ -46,7 +63,7 @@ export default class Task_List extends Component {
             }
         })
 
-        this.setState({ tasks })
+        this.setState({ tasks }, this.filter_tasks)
     }
 
     render() {
@@ -54,16 +71,18 @@ export default class Task_List extends Component {
         return (
             <View style={styles.container}>
                 <ImageBackground source={today_Image} style={styles.background}>
-                <View style={styles.icon_bar}>
-
-                </View>
+                    <View style={styles.icon_bar}>
+                        <TouchableOpacity onPress={this.toggle_filter}>
+                            <Icon name={this.state.show_done_task ? 'eye' : 'eye-slash'} size={20} color="#FFF"></Icon>
+                        </TouchableOpacity>
+                    </View>
                     <View style={styles.titleBar}>
                         <Text style={styles.title}>Hoje</Text>
                         <Text style={styles.subTitle}>{today}</Text>
                     </View>
                 </ImageBackground>
                 <View style={styles.taskList}>
-                    <FlatList data={this.state.tasks}
+                    <FlatList data={this.state.visible_tasks}
                         keyExtractor={item => `${item.id}`}
                         renderItem={({ item }) => <Task{...item} toggle_task={this.toggle_task} />} />
                     {/* <Task description={"Comprar jogo GTA 6"} estimate_at={new Date()} done_at={new Date()} />
@@ -118,8 +137,11 @@ const styles = StyleSheet.create({
         marginLeft: 20,
         marginBottom: 30
     },
-    icon_bar:{
-        flexDirection: 'row'
+    icon_bar: {
+        flexDirection: 'row',
+        marginHorizontal: 20,
+        marginTop: 50,
+        justifyContent: 'flex-end'
     },
 
 })
