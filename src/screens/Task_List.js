42e12b1233539ae166d413b92c3599f22667c2ad
aizelli <alexandre.izelli@gmail.com>
2024-06-14 08:10:18 -0300

    alteração TaskList

--- src/screens/Task_List.js
+++ src/screens/Task_List.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react"
 import { StyleSheet, View, Text, ImageBackground } from "react-native"
+import moment from "moment"
+import 'moment/locale/pt-br'
 
 import today_Image from '../../assets/imgs/today.jpg'
 
 export default class Task_List extends Component {
     render() {
+        const today = moment().locale('pt-br').format('dddd, DD [de] MMMM')
         return (
             <View style={styles.container}>
                 <ImageBackground source={today_Image} style={styles.background}>
-
+                    <View style={styles.titleBar}>
+                        <Text style={styles.title}>Hoje</Text>
+                        <Text style={styles.subTitle}>{today}</Text>
+                    </View>
                 </ImageBackground>
                 <View style={styles.taskList}>
                     <Text>Lista de Tarefas</Text>
@@ -21,11 +27,29 @@ export default class Task_List extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1
-    }, 
-    background:{
+    },
+    background: {
         flex: 3
     },
-    taskList:{
+    taskList: {
         flex: 7
+    },
+    titleBar: {
+        flex: 1,
+        justifyContent: 'flex-end'
+    },
+    title: {
+        fontFamily: 'Arial',
+        fontSize: 50,
+        color: '#FFF',
+        marginLeft: 20,
+        marginBottom: 20
+    },
+    subTitle: {
+        fontFamily: 'Arial',
+        fontSize: 20,
+        color: '#FFF',
+        marginLeft: 20,
+        marginBottom: 30
     }
 })
