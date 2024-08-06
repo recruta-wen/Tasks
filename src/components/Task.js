b5d0892d8b1cce907caf935dcf9eee1e5ed711b7
aizelli <alexandre.izelli@gmail.com>
2024-08-06 07:16:28 -0300

    alterações

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,7 +4,9 @@ import moment from "moment"
 import Icon from "react-native-vector-icons/FontAwesome6"
 
 export default props => {
-    const done_or_not = props.done_at != null ? {textDecorationLine: "line-through"}:{} 
+    const done_or_not = props.done_at != null ? { textDecorationLine: "line-through" } : {}
+    const date = props.done_at != null ? props.done_at : props.estimate_at
+    const formated_date = moment(date).format('ddd, D [de] MMM [de] YYYY')
     return (
         <View style={styles.container}>
             <TouchableWithoutFeedback>
@@ -14,8 +16,7 @@ export default props => {
             </TouchableWithoutFeedback>
             <View>
                 <Text style={[styles.desc, done_or_not]}>{props.description}</Text>
-                <Text style={styles.date}>{props.estimate_at + ""}</Text>
-                <Text style={styles.date}>{props.done_at + ""}</Text>
+                <Text style={styles.date}>{formated_date}</Text>
             </View>
         </View>
     )
@@ -28,13 +29,11 @@ function get_Check_View(done_at) {
                 <Icon name="check" size={20} color="#FFF"></Icon>
             </View>
         )
-
     } else {
         return (
             <View style={styles.pending}>
             </View>
         )
-
     }
 }
 
